4036787168fcdefc3323d87ab6a84303dbe28abe
Ligangmuhseen <triplerainbow07@gmail.com>
2023-12-17 19:57:11 +0300

    first committt

---
+++ .freeCodeCamp/reset.sh
@@ -0,0 +1,5 @@
+cp ./.freeCodeCamp/reset_files/questionnaire.sh .
+cp ./.freeCodeCamp/reset_files/countdown.sh .
+cp ./.freeCodeCamp/reset_files/bingo.sh .
+cp ./.freeCodeCamp/reset_files/fortune.sh .
+cp ./.freeCodeCamp/reset_files/five.sh .
---
+++ .freeCodeCamp/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Program to run my other four programs
+
+ ./questionnaire.sh
+
+ ./countdown.sh a
+
+ ./bingo.sh
+ ./fortune.sh
+
+
+
+
